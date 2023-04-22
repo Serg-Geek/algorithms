84639d916cd42bd1e57ae8ea4455928da0d70c26
Serg-Geek <110627203+Serg-Geek@users.noreply.github.com>
2023-04-22 23:40:40 +0400

    Create reverseLinkedlist.py

---
+++ reverseLinkedlist.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def reverse(self):
+        prev = None
+        current = self.head
+        while(current is not None):
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        self.head = prev
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.data)
+            temp = temp.next
+
+llist = LinkedList()
+llist.push(20)
+llist.push(4)
+llist.push(15)
+llist.push(85)
+llist.push(1)
+llist.push(-5)
+llist.push(0)
+
+print("Исходный список:")
+llist.printList()
+
+llist.reverse()
+
+print("Развернутый список:")
+llist.printList()        
